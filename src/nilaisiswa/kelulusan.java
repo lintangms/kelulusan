f7f514c7dd7e95dcd0e30e52d2e46d3087a96a81
Lintang MS <lintangmuhammadshiddiq@gmail.com>
2022-02-07 19:20:38 +0700

    Kelulusan siswa

---
+++ src/nilaisiswa/kelulusan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nilaisiswa;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class kelulusan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    siswa object = new siswa();
+    
+    object.ingfo();
+    object.Nilai();
+        }
+   
+        
+    }
+        
---
+++ src/nilaisiswa/siswa.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nilaisiswa;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class siswa {
+String Nama, Kelas;
+int Nilai;
+
+Scanner input = new Scanner(System.in);
+
+void ingfo(){
+    System.out.println("===== INGFO KELULUSAN ANDA =====");
+    System.out.println("Nama anda : ");
+    Nama = input.nextLine();
+    System.out.println("Kelas anda : ");
+    Kelas = input.nextLine();
+    System.out.println("===== TUNGGU SEBENTAR ======");
+    
+    
+}
+void Nilai(){
+    System.out.println("Masukan nilai anda : ");
+    Nilai = input.nextInt();
+    
+    if (Nilai >= 80){
+        System.out.println("Selamat anda lulus");
+        System.out.println("Ibumu pasti bangga");
+    }
+    else{
+        System.out.println("Kok nilainya kurang bang?");
+    }
+}
+    
+    }
+
+
+    
